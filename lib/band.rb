b59cbd6624413eec3c53b2eab342093f7e7195c4
Alec <incubusalec@gmail.com>
2015-05-15 18:01:22 +0000

    Add unit test for band name titlecasing. Passes spec.

--- lib/band.rb
+++ lib/band.rb
@@ -1,4 +1,12 @@
 class Band < ActiveRecord::Base
   has_and_belongs_to_many :venues
   validates :name, {presence: true, uniqueness: { case_sensitive: false}}
+
+  before_validation :normalize_name, on: :create
+
+  private
+    def normalize_name
+      self.name = self.name.downcase.titleize
+    end
+
 end
--- spec/band_spec.rb
+++ spec/band_spec.rb
@@ -6,4 +6,9 @@ describe Band do
     band = Band.new(name: 'Poison Sauce')
     expect(band.save).to eq(false)
   end
+
+  it('will change the name of the band to titlecase on creation') do
+   band = Band.create({name: "iNcuBuS"})
+   expect(band.name).to eq("Incubus")
+ end
 end
