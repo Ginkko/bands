6e7ef9a46698268cfa92d51adaa1894532143cb6
Alec <incubusalec@gmail.com>
2015-05-15 18:47:00 +0000

    Update styling. Change link color pallete.

--- lib/band.rb
+++ lib/band.rb
@@ -1,8 +1,9 @@
 class Band < ActiveRecord::Base
   has_and_belongs_to_many :venues
-  validates :name, {presence: true, uniqueness: { case_sensitive: false}}
   before_validation :normalize_name, on: :create
-  before_validation :normalize_name, on: :update
+  after_validation :normalize_name, on: :update
+  validates :name, {presence: true, uniqueness: { case_sensitive: false}}
+
   private
     def normalize_name
       self.name = self.name.downcase.titleize
